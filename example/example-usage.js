1d3d1375872f63563d79480a2b76edf84fcf018a
Taka Okunishi <okunishinishi@gmail.com>
2017-02-17 14:24:47 +0900

    Initial commit

---
+++ example/example-usage.js
@@ -0,0 +1,3 @@
+'use strict'
+
+const sugoClient = require('sugo-client')
---
+++ test/sugo_client_test.js
@@ -0,0 +1,56 @@
+/**
+ * Test case for sugoClient.
+ * Runs with mocha.
+ */
+'use strict'
+
+const SugoClient = require('../lib/sugo_client.js')
+const sgSocket = require('sg-socket')
+const assert = require('assert')
+const aport = require('aport')
+const asleep = require('asleep')
+const co = require('co')
+
+describe('sugo-client', function () {
+  this.timeout(13000)
+  let io, port
+  before(() => co(function * () {
+    port = yield aport()
+    io = sgSocket(port)
+    io.on('connection', (socket) => {
+      socket.on('foo', (data) => {
+        console.log('foo', data)
+        socket.emit('foores', data)
+      })
+      socket.on('error', (err) => {
+        console.error(err)
+      })
+      socket.on('close', () => {
+        console.log('close')
+      })
+    })
+  }))
+
+  after(() => co(function * () {
+    yield asleep(100)
+    io.close()
+    yield asleep(100)
+  }))
+
+  it('Sugo client', () => co(function * () {
+    let client = new SugoClient()
+    assert.ok(client)
+    let socket = yield client.newSocket(`http://localhost:${port}`)
+    socket.on('error', (err) => {
+      console.error(err)
+    })
+    socket.on('foores', (data) => {
+      console.log('!!!!!!!!!')
+    })
+    socket.emit('foo', { msg: 'This is foo' })
+    asleep(1500)
+    yield client.destroySocket(socket)
+  }))
+})
+
+/* global describe, before, after, it */
